2e8de66bb4265f2234cb35da1a27879bf807991c
AmitG7781 <amit@paywithatoa.co.uk>
2023-12-27 23:46:22 +0530

    added assertion methods

--- src/main/java/web/genericLibraries/BaseClass.java
+++ src/main/java/web/genericLibraries/BaseClass.java
@@ -1,22 +1,25 @@
 package web.genericLibraries;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
+import org.testng.asserts.SoftAssert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import web.utilities.Initialization;
 import web.utilities.PropertyFile;
 import web.utilities.WebDriverUtilities;
 
@@ -24,116 +27,63 @@ public class BaseClass
 {
 	public WebDriver driver;
 	public PropertyFile pdata=new PropertyFile();
+	public SoftAssert sa= new SoftAssert();
 	public WebDriverUtilities utilities=new WebDriverUtilities();
-	public List<String> uatBankPageTitles= new ArrayList<String>();
-	@BeforeSuite
-	public void bankPageTitleList() throws IOException
-	{
-		uatBankPageTitles.add(pdata.getPropertydata("hsbc"));
-		uatBankPageTitles.add(pdata.getPropertydata("barclays"));
-		uatBankPageTitles.add(pdata.getPropertydata("monzo"));
-		uatBankPageTitles.add(pdata.getPropertydata("nationwide"));
-		uatBankPageTitles.add(pdata.getPropertydata("natwest"));
-		uatBankPageTitles.add(pdata.getPropertydata("rbos"));
-		uatBankPageTitles.add(pdata.getPropertydata("santander"));
-		uatBankPageTitles.add(pdata.getPropertydata("lloyds"));
-		uatBankPageTitles.add(pdata.getPropertydata("revolut"));
-		uatBankPageTitles.add(pdata.getPropertydata("bosc"));
-		uatBankPageTitles.add(pdata.getPropertydata("halifax"));
-		uatBankPageTitles.add(pdata.getPropertydata("starling"));
-		uatBankPageTitles.add(pdata.getPropertydata("tsb"));
-		uatBankPageTitles.add(pdata.getPropertydata("fd"));
-	}
+	public List<String> bankPageTitles= new ArrayList<String>();
+	public List<String> occupationOptions= new ArrayList<String>();
+	public List<String> sourceOfIncome= new ArrayList<String>();
+	public List<String> nationality= new ArrayList<String>();
 	
-	@Parameters({"env", "paymentAcceptMode", "browserName"})
+	@Parameters({"env", "paymentMode", "browserName", "merchant"})
 	@BeforeMethod
-	public void open(String env, String paymentAcceptMode, String browserName) throws IOException
+	public void open(String env, String paymentMode, String browser, String merchant) throws IOException
 	{
-		if(browserName.equalsIgnoreCase("chrome"))
+		if(browser.equalsIgnoreCase("chrome"))
 		{ 
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("headless");
 			WebDriverManager.chromedriver().setup();
 			driver=new ChromeDriver(); 
 		}
-		else if(browserName.equalsIgnoreCase("firefox"))
+		else if(browser.equalsIgnoreCase("firefox"))
 		{
+			FirefoxOptions options=new FirefoxOptions();
+			options.addArguments("--headless");
 			WebDriverManager.firefoxdriver().setup();
 			driver=new FirefoxDriver();
 		}
-		else if(browserName.equalsIgnoreCase("edge"))
+		else if(browser.equalsIgnoreCase("edge"))
 		{
+			EdgeOptions options=new EdgeOptions();
+			options.addArguments("--headless");
 			WebDriverManager.edgedriver().setup();
 			driver=new EdgeDriver();
 		}
 		
 		driver.manage().window().maximize();
 		
-		if(env.equalsIgnoreCase("DEV"))
+		if(paymentMode.equalsIgnoreCase("dynamicQR"))
 		{
-			if(paymentAcceptMode.equalsIgnoreCase("dynamicQR"))
-			{
-				driver.navigate().to(pdata.getPropertydata("devDynamicQR"));
-			}
-			else if(paymentAcceptMode.equalsIgnoreCase("link"))
-			{
-				driver.navigate().to(pdata.getPropertydata("devLink"));
-			}
-			else if(paymentAcceptMode.equalsIgnoreCase("physicalQR"))
-			{
-				driver.navigate().to(pdata.getPropertydata("devPhysicalQR"));
-			}
-			else if(paymentAcceptMode.equalsIgnoreCase("staticQR"))
-			{
-				driver.navigate().to(pdata.getPropertydata("devStaticQR"));
-			}
-			else if(paymentAcceptMode.equalsIgnoreCase("cashbackLink"))
-			{
-				driver.navigate().to(pdata.getPropertydata("cashbackPaymentLink"));
-			}
+			driver.navigate().to(pdata.getPropertydata(env+"DynamicQR"+merchant));
+		}
+		else if(paymentMode.equalsIgnoreCase("link"))
+		{
+			driver.navigate().to(pdata.getPropertydata(env+"Link"+merchant));
+		}
+		else if(paymentMode.equalsIgnoreCase("physicalQR"))
+		{
+			driver.navigate().to(pdata.getPropertydata(env+"PhysicalQR"+merchant));
+		}
+		else if(paymentMode.equalsIgnoreCase("staticQR"))
+		{
+			driver.navigate().to(pdata.getPropertydata(env+"StaticQR"+merchant));
 		}
 		
-		else if(env.equalsIgnoreCase("UAT"))
-		{
-			if(paymentAcceptMode.equalsIgnoreCase("dynamicQR"))
-			{
-				driver.navigate().to(pdata.getPropertydata("uatDynamicQR"));
-			}
-			else if(paymentAcceptMode.equalsIgnoreCase("link"))
-			{
-				driver.navigate().to(pdata.getPropertydata("uatLink"));
-			}
-			else if(paymentAcceptMode.equalsIgnoreCase("physicalQR"))
-			{
-				driver.navigate().to(pdata.getPropertydata("uatPhysicalQR"));
-			}
-			else if(paymentAcceptMode.equalsIgnoreCase("staticQR"))
-			{
-				driver.navigate().to(pdata.getPropertydata("uatStaticQR"));
-			}
-		}
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+		driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(15));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
 		
-		else if(env.equalsIgnoreCase("PROD"))
-		{
-			if(paymentAcceptMode.equalsIgnoreCase("dynamicQR"))
-			{
-				driver.navigate().to(pdata.getPropertydata("prodDynamicQR"));
-			}
-			else if(paymentAcceptMode.equalsIgnoreCase("link"))
-			{
-				driver.navigate().to(pdata.getPropertydata("prodLink"));
-			}
-			else if(paymentAcceptMode.equalsIgnoreCase("physicalQR"))
-			{
-				driver.navigate().to(pdata.getPropertydata("prodPhysicalQR"));
-			}
-			else if(paymentAcceptMode.equalsIgnoreCase("staticQR"))
-			{
-				driver.navigate().to(pdata.getPropertydata("prodStaticQR"));
-			}
-		}
-		
-		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		new Initialization().initialization();
 	}
 
 	
@@ -146,7 +96,7 @@ public class BaseClass
 	@AfterTest
 	public void assertAllMethods()
 	{
-		sa.assertAll();
+		
 	}
  
 }
--- src/main/java/web/pomPages/BanksPage.java
+++ src/main/java/web/pomPages/BanksPage.java
@@ -2,10 +2,12 @@ package web.pomPages;
 
 import java.util.List;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 public class BanksPage {
 
@@ -93,4 +95,223 @@ public class BanksPage {
 	
 	@FindBy (id="bank-list-item-rbs-sandbox")
 	private WebElement rbs;
+	
+	public BanksPage(WebDriver driver) 
+	{
+		PageFactory.initElements(driver, this);
+	}
+
+
+	public WebElement getHeaderLogo() {
+		return headerLogo;
+	}
+
+
+	public WebElement getStoreImage() {
+		return storeImage;
+	}
+
+
+	public WebElement getPayingTitle() {
+		return payingTitle;
+	}
+	
+	
+	public String getPayingTitleText() {
+		return payingTitle.getText();
+	}
+
+
+	public WebElement getBusinessName() {
+		return businessName;
+	}
+	
+	
+	public String getBusinessNameText() {
+		return businessName.getText();
+	}
+
+	
+	public WebElement getStoreName() {
+		return storeName;
+	}
+
+	
+	public String getStoreNameText() {
+		return storeName.getText();
+	}
+
+	public WebElement getTotalAmount() {
+		return totalAmount;
+	}
+	
+	public String getTotalAmountText() {
+		return totalAmount.getText();
+	}
+
+	public WebElement getAmountEditButton() {
+		return amountEditButton;
+	}
+
+	public void clickAmountEditButton() {
+		 amountEditButton.click();
+	}
+	
+	public WebElement getIncludesTip() {
+		return includesTipText;
+	}
+	
+	public String getIncludesTipText() {
+		return includesTipText.getText();
+	}
+
+	public WebElement getSelectBank() {
+		return selectBankText;
+	}
+	
+	public String getSelectBankText() {
+		return selectBankText.getText();
+	}
+
+	public WebElement getViewAllBanksButton() {
+		return viewAllBanksButton;
+	}
+	
+	public void clickViewAllBanksButton() {
+		 viewAllBanksButton.click();
+	}
+
+	public List<WebElement> getBankTabsContainer() {
+		return bankTabsContainer;
+	}
+
+	public WebElement getHowToPayWithAtoaButton() {
+		return howToPayWithAtoaButton;
+	}
+	
+	public void clickHowToPayWithAtoaButton() {
+		 howToPayWithAtoaButton.click();
+	}
+
+	public WebElement getHowItWorksImage() {
+		return howItWorksImage;
+	}
+
+	public WebElement getTermsCheckbox() {
+		return termsCheckbox;
+	}
+	
+	public void clickTermsCheckbox() {
+		 termsCheckbox.click();
+	}
+
+	public WebElement getTermsLink() {
+		return termsLink;
+	}
+	
+	public void clickTermsLink() {
+		 termsLink.click();
+	}
+
+	public WebElement getPrivacyPolicyLink() {
+		return privacyPolicyLink;
+	}
+	
+	public void clickPrivacyPolicyLink() {
+		 privacyPolicyLink.click();
+	}
+
+	public WebElement getHelpLink() {
+		return helpLink;
+	}
+	
+	public void clickHelpLink() {
+		 helpLink.click();
+	}
+
+	public WebElement getPoweredBy() {
+		return poweredBy;
+	}
+	
+	public String getPoweredByText() {
+		return poweredBy.getText();
+	}
+
+	public WebElement getProceedButton() {
+		return proceedButton;
+	}
+	
+	public void clickProceedButton() {
+		 proceedButton.click();
+	}
+
+	public WebElement getToast() {
+		return toastText;
+	}
+	
+	public String getToastText() {
+		return toastText.getText();
+	}
+
+	public WebElement getToastClose() {
+		return toastClose;
+	}
+
+	public void clickToastClose() {
+		 toastClose.click();
+	}
+	
+	public WebElement getAllBanksPageHeader() {
+		return allBanksPageHeader;
+	}
+	
+	public String getAllBanksPageHeaderText() {
+		return allBanksPageHeader.getText();
+	}
+
+	public WebElement getBanksSearchBox() {
+		return banksSearchBox;
+	}
+	
+	public void searchBank(String key) {
+		 banksSearchBox.sendKeys(key);
+	}
+
+	public List<WebElement> getAllBankList() {
+		return allBankList;
+	}
+
+	public WebElement getNoBanksMessage() {
+		return noBanksMessage;
+	}
+	
+	public String getNoBanksMessageText() {
+		return noBanksMessage.getText();
+	}
+
+	public WebElement getLloyds() {
+		return lloyds;
+	}
+	
+	public void clickLloyds() {
+		 lloyds.click();;
+	}
+
+	public WebElement getModelo() {
+		return modelo;
+	}
+	
+	public void clickModelo() {
+		 modelo.click();
+	}
+
+	public WebElement getRbs() {
+		return rbs;
+	}
+	
+	public void clickRbs() {
+		 rbs.click();
+	}
+	
+	
 }
